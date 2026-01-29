e4c8d42d065252982a7c532f43ac7ef301cec627
ayan-goel <agoel320@gatech.edu>
2026-01-28 21:22:25 -0500

    re-enable groupthink comment feature

--- client/src/pages/RusheeZoom.jsx
+++ client/src/pages/RusheeZoom.jsx
@@ -609,7 +609,7 @@ export default function RusheeZoom() {
                                 </div>
 
                                 {/* Section: Brothers who wrote comments */}
-                                {true && ( // Temporarily disabled: hasUserPostedComment() && (
+                                {hasUserPostedComment() && (
                                     <div className="card-apple p-6 mb-6">
                                         <h2 className="text-apple-title1 font-light text-black mb-4">
                                             Brothers Who Commented
@@ -696,7 +696,7 @@ export default function RusheeZoom() {
                                     )}
                                     
                                     {/* Show comments only if user has posted their own comment */}
-                                    {true && ( // Temporarily disabled: hasUserPostedComment() && (
+                                    {hasUserPostedComment() && (
                                         <div className="mt-6 space-y-4">
                                             {rushee.comments.map((comment, idx) => (
                                                 <div
@@ -791,10 +791,10 @@ export default function RusheeZoom() {
                                     )}
 
                                     {/* Show message if user hasn't posted a comment yet */}
-                                    {false && ( // Temporarily disabled: !hasUserPostedComment() && (
+                                    {!hasUserPostedComment() && (
                                         <div className="mt-6 p-6 bg-apple-gray-50 border border-apple-gray-200 rounded-apple text-center">
                                             <p className="text-apple-body text-apple-gray-600 font-light">
-                                                Post your first comment to see other brothers' comments.
+                                                Post your comment to see other brothers' comments.
                                             </p>
                                         </div>
                                     )}
